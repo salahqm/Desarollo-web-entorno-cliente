974b07d3c5bf99e00d36cc3f27025f24ea5d2cc2
salah <selqaddoury01@educarex.es>
2024-10-15 12:40:30 +0200

    json nuevo

---
+++ ajax/tarea1/script.js
@@ -0,0 +1,43 @@
+window.addEventListener('load', () => {
+
+
+    const url = "http://camacho.atwebpages.com/webcam/getWebcam.php"
+
+    const ubicacion = document.querySelector('#fila');
+    fetch(url)
+        .then(response => response.text())
+        .then(datos => mostrarDatos(datos))
+        .finally(() => alert("biennnnnnnnn"))
+        .catch(error => alert(error.data))
+
+
+    function mostrarDatos(datos) {
+
+        datos.forEach(webc => {
+            const columna = document.createElement('div')
+            columna.className = "col-lg-4"
+            ubicacion.appendChild(columna)
+            const tarjeta = document.createElement('div');
+            tarjeta.className="card"
+            columna.appendChild(tarjeta)
+            const videoW= document.createElement('video');
+            videoW.setAttribute('src',webc.url)
+            tarjeta.appendChild(videoW)
+            const cardBody = document.createElement('div')
+            cardBody.className="card-body"
+            tarjeta.appendChild(cardBody);
+            const titulo = document.createElement('h4');
+            titulo.className='card-title';
+            titulo.textContent=webc.lugar;
+            cardBody.appendChild(titulo)
+            const subtitulo= document.createElement('p')
+            subtitulo.className='card-text';
+            subtitulo.textContent=webc.tiempo
+            cardBody.appendChild(subtitulo)
+
+
+
+        });
+    }
+
+})
