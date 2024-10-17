4cf3705f0d4cb94f58b5f007bae64b7a6b8b1dcd
Salah el qaddoury <Selqaddoury01@educarex.es>
2024-10-17 11:51:25 +0200

    terminar en casa

--- ajax/Tarea3_16Octubre/js/script.js
+++ ajax/Tarea3_16Octubre/js/script.js
@@ -3,8 +3,6 @@ window.addEventListener("load", () => {
   const caja = document.querySelector("div");
   const mostrar = document.getElementById("mostrar");
 
- 
-
   fetch(url)
     .then((response) => response.json())
     .then((datos) => mostrarDatos(datos))
@@ -15,25 +13,24 @@ window.addEventListener("load", () => {
 
   function mostrarDatos(datos) {
     mostrar.addEventListener("click", () => {
-        
-          intervalo = setInterval(() => {
-            // Limpiamos el contenedor antes de agregar un nuevo video para que se ve solo un video
-            while (caja.firstChild) {
-              caja.removeChild(caja.firstChild);
-            }
-    
-           // creamos un num aleatorio para poder selecionar los videos
-            let num = Math.floor(Math.random() * datos.length);
-            const item = datos[num]; 
-    
-            // Crear el elemento de video
-            const video = document.createElement("video");
-            video.setAttribute("src", item.url);
-            video.setAttribute("autoplay",true);
-            caja.appendChild(video);
-    
-            video.style.width = "100%";
-          }, 10000); 
+      intervalo = setInterval(() => {
+        // Limpiamos el contenedor antes de agregar un nuevo video para que se ve solo un video
+        while (caja.firstChild) {
+          caja.removeChild(caja.firstChild);
+        }
+
+        // creamos un num aleatorio para poder selecionar los videos
+        let num = Math.floor(Math.random() * datos.length);
+        const item = datos[num];
+
+        // Crear el elemento de video
+        const video = document.createElement("video");
+        video.setAttribute("src", item.url);
+        video.setAttribute("autoplay", true);
+        caja.appendChild(video);
+
+        video.style.width = "100%";
+      }, 10000);
     });
   }
 });
