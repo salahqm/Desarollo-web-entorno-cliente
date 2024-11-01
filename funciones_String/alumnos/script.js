cf901d251a1f4543871fa32b29149f56fd2b6c1c
salah <selqaddoury01@educarex.es>
2024-11-01 14:07:35 +0100

    tarea alumnos

---
+++ funciones_String/alumnos/script.js
@@ -0,0 +1,88 @@
+window.addEventListener("DOMContentLoaded", inicio);
+
+function inicio() {
+  const contenedor = document.querySelector(".col-lg-12");
+  const tabla = document.createElement("table");
+  tabla.className = "table";
+  const tbody = document.createElement("tbody");
+  const thead = document.createElement("thead");
+  thead.className = "thead-dark"; 
+
+  contenedor.appendChild(tabla);
+  tabla.appendChild(thead);
+  tabla.appendChild(tbody);
+
+  const url = "alumnos.csv";
+  fetch(url)
+    .then((response) => response.text())
+    .then((data) => {
+      mostrarDatos(data);
+    });
+
+  function mostrarDatos(data) {
+    let cabecera = [];
+    let separadorFilas = [];
+    let separadorColumnas = [];
+
+    separadorFilas = data.split("\n");
+
+    const tr = document.createElement("tr");
+    thead.appendChild(tr);
+    cabecera = separadorFilas[0].split(",");
+    cabecera.forEach((element) => {
+      const th = document.createElement("th");
+      th.textContent = element;
+      tr.appendChild(th);
+    });
+
+    separadorFilas.forEach((element, i) => {
+      if (i > 0) {
+        separadorColumnas = element.split(",");
+        const tr1 = document.createElement("tr");
+        tbody.appendChild(tr1);
+        separadorColumnas.forEach((element) => {
+          const td = document.createElement("td");
+          td.textContent = element;
+          tr1.appendChild(td);
+        });
+      }
+    });
+
+    const tdTodos = document.querySelectorAll("tr");
+    let contador = 0;
+    tdTodos.forEach((tr, i) => {
+      
+      const celdas = tr.querySelectorAll("td");
+      if (i > 0) {
+        celdas.forEach((td, index) => {
+          if (index > 1) {
+            if (Number(td.textContent) >= 5) {
+              contador++;
+            }
+          }
+        });
+      }
+      switch (contador) {
+        case 5:
+          tr.style.backgroundColor = "green";
+          break;
+        case 4:
+          tr.style.backgroundColor = "blue";
+          break;
+        case 3:
+          tr.style.backgroundColor = "yellow";
+          break;
+        case 2:
+          tr.style.backgroundColor = "pink";
+          break;
+        case 1:
+          tr.style.backgroundColor = "orange";
+          break;
+        default:
+          tr.style.backgroundColor = "red";
+          break;
+      }
+       contador = 0;
+    });
+  }
+}
