9c39e85729208bc18fbbbce8817ff62f9dd63d44
salah <selqaddoury01@educarex.es>
2024-11-15 12:46:59 +0100

    terminar en casa

---
+++ Angular/ejercicio4/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ejercicio4';
+  euros:number=0
+  dolares:number=0
+  yenes:number=0
+
+}
