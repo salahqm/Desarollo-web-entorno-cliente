f46a18677b564b453f4209ca60fa9dfb8a07a216
salah <selqaddoury01@educarex.es>
2025-01-14 23:29:54 +0100

    terminar en clase

--- Angular/concesionario/src/app/app.module.ts
+++ Angular/concesionario/src/app/app.module.ts
@@ -4,15 +4,60 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactivoComponent } from './reactivo/reactivo.component';
+import { FormsModule, ReactiveFormsModule, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatOptionModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReactivoComponent
+    ReactivoComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatDividerModule,
+    MatNativeDateModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatDividerModule,
+    MatNativeDateModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatRadioModule
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/concesionario/src/app/reactivo/reactivo.component.ts
+++ Angular/concesionario/src/app/reactivo/reactivo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactivo',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./reactivo.component.css']
 })
 export class ReactivoComponent {
+  enviar() {
+    throw new Error('Method not implemented.');
+  }
+  ConcesionarioRegistro!: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.ConcesionarioRegistro = this.fb.group({
+      matricula: ['', [Validators.required, Validators.pattern('^\\d{4}[A-Z]{3}$')]],
+      opcion: ['', [Validators.required]],
+      neumaicos: ['', [Validators.required]]
+    });
+  }
 
 }
